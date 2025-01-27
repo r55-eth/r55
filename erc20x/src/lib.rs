c4e8b7b2103ce7e79483b891350e4e145d192626
0xrusowsky <0xrusowsky@proton.me>
2025-01-27 22:04:59 +0100

    comment fail example

--- erc20x/src/lib.rs
+++ erc20x/src/lib.rs
@@ -16,7 +16,7 @@ pub struct ERC20x;
 #[contract]
 impl ERC20x {
     pub fn x_balance_of(&self, owner: Address, target: Address) -> U256 {
-        let token = IERC20::new(target).with_ctx(self);
+        let token = IERC20::new(target).with_ctx(self);         // IERC20<ReadOnly>
         match token.balance_of(owner) {
             Some(balance) => balance,
             _ => eth_riscv_runtime::revert(),
@@ -24,10 +24,18 @@ impl ERC20x {
     }
 
     pub fn x_mint(&mut self, owner: Address, target: Address) -> bool {
-        let mut token = IERC20::new(target).with_ctx(self);
+        let mut token = IERC20::new(target).with_ctx(self);     // IERC20<ReadWrite>
         match token.mint(owner, U256::from(1000000000000000000_u128)) {
             Some(success) => success,
             _ => eth_riscv_runtime::revert()
         }
     }
+
+    // pub fn x_mint_fails(&self, owner: Address, target: Address) -> bool {
+    //     let mut token = IERC20::new(target).with_ctx(self);  // IERC20<ReadOnly>
+    //     match token.mint(owner, U256::from(1000000000000000000_u128)) {
+    //         Some(success) => success,
+    //         _ => eth_riscv_runtime::revert()
+    //     }
+    // }
 }
