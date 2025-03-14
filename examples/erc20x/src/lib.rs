094d74ba41b4da9e979ffb8c6cad27e8dfac2eb5
0xrusowsky <0xrusowsky@proton.me>
2025-03-14 08:07:10 +0100

    automate deployable creation

--- examples/erc20x/src/lib.rs
+++ examples/erc20x/src/lib.rs
@@ -18,10 +18,7 @@ pub struct ERC20x;
 
 #[contract]
 impl ERC20x {
-    pub fn x_bytecode(&mut self) -> Bytes {
-        ERC20::bytecode()
-    }
-
+    // Deploys a new ERC20 token instance
     pub fn x_deploy(&mut self, owner: Address) -> (Address, Address) {
         let token = ERC20::deploy(owner).with_ctx(self);            // IERC20<ReadWrite>
         let owner = token.owner().expect("Unable to get owner");
@@ -29,21 +26,25 @@ impl ERC20x {
         (token.address(), owner)
     }
 
+    // Performs a staticcall to an ERC20
     pub fn x_balance_of(&self, owner: Address, token_addr: Address) -> Option<U256> {
         let token = IERC20::new(token_addr).with_ctx(self);         // IERC20<ReadOnly>
         token.balance_of(owner)
     }
 
+    // Performs a (mutable) call to an ERC20
     pub fn x_mint(&mut self, to: Address, amount: U256, token_addr: Address) -> Result<bool, ERC20Error> {
         let mut token = IERC20::new(token_addr).with_ctx(self);     // IERC20<ReadWrite>
         token.mint(to, amount)
     }
 
+    // Fails to perform a (mutable) call to an ERC20, due to the lack of mutability in the ERC20x method
     // pub fn x_mint_fails(&self, to: Address, token_addr: Address) -> Result<bool, ERC20Error> {
     //     let mut token = IERC20::new(token_addr).with_ctx(self);  // IERC20<ReadOnly>
     //     token.mint(to, amount)
     // }
 
+    // If the call fails, it re-attemps another call based on the error type. 
     pub fn x_transfer_from(
         &mut self,
         from: Address,
@@ -61,8 +62,10 @@ impl ERC20x {
         }
     }
 
+    // Always reverts with a str msg
     pub fn panics(&self) { panic!("This function always panics"); }
 
+    // If the call fails, reverts with a str msg
     pub fn x_mint_panics(&mut self, to: Address, amount: U256, token_addr: Address) -> bool {
         let mut token = IERC20::new(token_addr).with_ctx(self);     // IERC20<ReadWrite>
         token.mint(to, amount).expect("ERC20::mint() failed!")
