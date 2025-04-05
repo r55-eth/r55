16276df26cf95a61b5381e3a2aa9026e45f014e8
0xrusowsky <0xrusowsky@proton.me>
2025-04-05 20:50:16 +0200

    style: fmt + clippy

--- r55/src/generated/mod.rs
+++ r55/src/generated/mod.rs
@@ -1,3 +1,4 @@
+
 //! This module contains auto-generated code.
 //! Do not edit manually!
 
@@ -5,8 +6,7 @@ use alloy_core::primitives::Bytes;
 use core::include_bytes;
 
 pub const ERC721_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/erc721.bin");
-pub const EVM_CALLER_BYTECODE: &[u8] =
-    include_bytes!("../../../r55-output-bytecode/evm-caller.bin");
+pub const EVM_CALLER_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/evm-caller.bin");
 pub const ERC20_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/erc20.bin");
 pub const ERC20X_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/erc20x.bin");
 
@@ -16,7 +16,7 @@ pub fn get_bytecode(contract_name: &str) -> Bytes {
         "evm_caller" => EVM_CALLER_BYTECODE,
         "erc20" => ERC20_BYTECODE,
         "erc20x" => ERC20X_BYTECODE,
-        _ => return Bytes::new(),
+        _ => return Bytes::new()
     };
 
     Bytes::from(initcode)
