03f7c052cb3afd0d567c6536125f75845d5249fa
0xrusowsky <0xrusowsky@proton.me>
2025-04-27 10:04:38 +0200

    style: fmt

--- r55/src/generated/mod.rs
+++ r55/src/generated/mod.rs
@@ -5,11 +5,14 @@ use alloy_core::primitives::Bytes;
 use core::include_bytes;
 
 pub const ERC721_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/erc721.bin");
-pub const EVM_CALLER_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/evm-caller.bin");
+pub const EVM_CALLER_BYTECODE: &[u8] =
+    include_bytes!("../../../r55-output-bytecode/evm-caller.bin");
 pub const ERC20_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/erc20.bin");
 pub const ERC20X_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/erc20x.bin");
-pub const UNISWAP_V2_PAIR_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/uniswap-v2-pair.bin");
-pub const UNISWAP_V2_FACTORY_BYTECODE: &[u8] = include_bytes!("../../../r55-output-bytecode/uniswap-v2-factory.bin");
+pub const UNISWAP_V2_PAIR_BYTECODE: &[u8] =
+    include_bytes!("../../../r55-output-bytecode/uniswap-v2-pair.bin");
+pub const UNISWAP_V2_FACTORY_BYTECODE: &[u8] =
+    include_bytes!("../../../r55-output-bytecode/uniswap-v2-factory.bin");
 
 pub fn get_bytecode(contract_name: &str) -> Bytes {
     let initcode = match contract_name {
